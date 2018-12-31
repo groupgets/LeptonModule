4a41e2f17fdf720d930358907c5df51413c61c56
Yutaka Yasuda <yyasuda@users.noreply.github.com>
2018-12-31 14:51:09 +0900

    Update Lepton_I2C.cpp
    Add lepton_reboot().
    (No return code check, it will return -26 and it is the sign it works correctly.)

--- software/raspberrypi_video/Lepton_I2C.cpp
+++ software/raspberrypi_video/Lepton_I2C.cpp
@@ -2,6 +2,7 @@
 
 #include "leptonSDKEmb32PUB/LEPTON_SDK.h"
 #include "leptonSDKEmb32PUB/LEPTON_SYS.h"
+#include "leptonSDKEmb32PUB/LEPTON_OEM.h"
 #include "leptonSDKEmb32PUB/LEPTON_Types.h"
 
 bool _connected;
@@ -22,3 +23,10 @@ void lepton_perform_ffc() {
 }
 
 //presumably more commands could go here if desired
+
+void lepton_reboot() {
+	if(!_connected) {
+		lepton_connect();
+	}
+	LEP_RunOemReboot(&_port);
+}
