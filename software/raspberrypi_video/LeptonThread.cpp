7e90a95fbb50c36008afe0e40ad57f8327b766aa
Toshio HIGUCHI <higuchi_toshio@intec.co.jp>
2019-09-10 11:08:17 +0900

    support Lepton 3.x

--- software/raspberrypi_video/LeptonThread.cpp
+++ software/raspberrypi_video/LeptonThread.cpp
@@ -16,6 +16,18 @@ LeptonThread::LeptonThread() : QThread()
 	typeColormap = 3; // 1:colormap_rainbow  /  2:colormap_grayscale  /  3:colormap_ironblack(default)
 	selectedColormap = colormap_ironblack;
 	selectedColormapSize = get_size_colormap_ironblack();
+
+	//
+	typeLepton = 2; // 2:Lepton 2.x  / 3:Lepton 3.x
+	myImageWidth = 80;
+	myImageHeight = 60;
+
+	//
+	spiSpeed = 20 * 1000 * 1000; // SPI bus speed 20MHz
+
+	// min/max value for scaling
+	rangeMin = 30000;
+	rangeMax = 32000;
 }
 
 LeptonThread::~LeptonThread() {
@@ -42,21 +54,56 @@ void LeptonThread::useColormap(int newTypeColormap)
 	}
 }
 
+void LeptonThread::useLepton(int newTypeLepton)
+{
+	switch (newTypeLepton) {
+	case 3:
+		typeLepton = 3;
+		myImageWidth = 160;
+		myImageHeight = 120;
+		break;
+	default:
+		typeLepton = 2;
+		myImageWidth = 80;
+		myImageHeight = 60;
+	}
+}
+
+void LeptonThread::useSpiSpeedMhz(unsigned int newSpiSpeed)
+{
+	spiSpeed = newSpiSpeed * 1000 * 1000;
+}
+
+void LeptonThread::useRangeMinValue(uint16_t newMinValue)
+{
+	rangeMin = newMinValue;
+}
+
+void LeptonThread::useRangeMaxValue(uint16_t newMaxValue)
+{
+	rangeMax = newMaxValue;
+}
+
 void LeptonThread::run()
 {
 	//create the initial image
-	myImage = QImage(80, 60, QImage::Format_RGB888);
+	myImage = QImage(myImageWidth, myImageHeight, QImage::Format_RGB888);
 
 	const int *colormap = selectedColormap;
 	const int colormapSize = selectedColormapSize;
+	uint16_t minValue = rangeMin;
+	uint16_t maxValue = rangeMax;
+	float diff = maxValue - minValue;
+	float scale = 255/diff;
 
 	//open spi port
-	SpiOpenPort(0);
+	SpiOpenPort(0, spiSpeed);
 
 	while(true) {
 
 		//read data packets from lepton over SPI
 		int resets = 0;
+		int segmentNumber = -1;
 		for(int j=0;j<PACKETS_PER_FRAME;j++) {
 			//if it's a drop packet, reset j to 0, set to -1 so he'll be at 0 again loop
 			read(spi_cs0_fd, result+sizeof(uint8_t)*PACKET_SIZE*j, sizeof(uint8_t)*PACKET_SIZE);
@@ -71,57 +118,57 @@ void LeptonThread::run()
 					SpiClosePort(0);
 					lepton_reboot();
 					usleep(750000);
-					SpiOpenPort(0);
+					SpiOpenPort(0, spiSpeed);
 				}
+				continue;
+			}
+			if(packetNumber == 20) {
+				segmentNumber = (result[j*PACKET_SIZE] >> 4) & 0x0f;
 			}
 		}
 		if(resets >= 30) {
 			qDebug() << "done reading, resets: " << resets;
 		}
 
+		if ((typeLepton == 3) && ((segmentNumber < 1) || (4 < segmentNumber))) {
+			continue;
+		}
+		int ofsRow = 30 * (segmentNumber - 1);
+
 		frameBuffer = (uint16_t *)result;
 		int row, column;
 		uint16_t value;
-		uint16_t minValue = 65535;
-		uint16_t maxValue = 0;
-
-		
+		QRgb color;
 		for(int i=0;i<FRAME_SIZE_UINT16;i++) {
 			//skip the first 2 uint16_t's of every packet, they're 4 header bytes
 			if(i % PACKET_SIZE_UINT16 < 2) {
 				continue;
 			}
-			
+
 			//flip the MSB and LSB at the last second
 			int temp = result[i*2];
 			result[i*2] = result[i*2+1];
 			result[i*2+1] = temp;
-			
-			value = frameBuffer[i];
-			if(value > maxValue) {
-				maxValue = value;
-			}
-			if(value < minValue) {
-				minValue = value;
-			}
-			column = i % PACKET_SIZE_UINT16 - 2;
-			row = i / PACKET_SIZE_UINT16 ;
-		}
 
-		float diff = maxValue - minValue;
-		float scale = 255/diff;
-		QRgb color;
-		for(int i=0;i<FRAME_SIZE_UINT16;i++) {
-			if(i % PACKET_SIZE_UINT16 < 2) {
-				continue;
+			if (frameBuffer[i] == 0) {
+				// Why this value is 0?
+				break;
 			}
+
+			//
 			value = (frameBuffer[i] - minValue) * scale;
 			int ofs_r = 3 * value + 0; if (colormapSize <= ofs_r) ofs_r = colormapSize - 1;
 			int ofs_g = 3 * value + 1; if (colormapSize <= ofs_g) ofs_g = colormapSize - 1;
 			int ofs_b = 3 * value + 2; if (colormapSize <= ofs_b) ofs_b = colormapSize - 1;
 			color = qRgb(colormap[ofs_r], colormap[ofs_g], colormap[ofs_b]);
-			column = (i % PACKET_SIZE_UINT16 ) - 2;
-			row = i / PACKET_SIZE_UINT16;
+			if (typeLepton == 3) {
+				column = (i % PACKET_SIZE_UINT16) - 2 + (myImageWidth / 2) * ((i % (PACKET_SIZE_UINT16 * 2)) / PACKET_SIZE_UINT16);
+				row = i / PACKET_SIZE_UINT16 / 2 + ofsRow;
+			}
+			else {
+				column = (i % PACKET_SIZE_UINT16) - 2;
+				row = i / PACKET_SIZE_UINT16;
+			}
 			myImage.setPixel(column, row, color);
 		}
 
